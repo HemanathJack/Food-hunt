f81ae315cd0a67361e8209929ad5861bd1328277
HemanathJack <rhemanath10@gmail.com>
2023-08-29 21:20:49 +0530

    alllowed host

--- Zoom_Recording_Display_Management/settings.py
+++ Zoom_Recording_Display_Management/settings.py
@@ -28,8 +28,9 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "FALSE").lower() == "true"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+#ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
+ALLOWED_HOSTS = ["https://django-food-recipe-webapp.onrender.com", "localhost", "127.0.0.1", "[::1]"]
 
 # Application definition
 
